277f45559eb061a8acbe43e7e8b6aaa891a9c3b3
neworderofjamie <neworderofjamie@gmail.com>
2023-02-14 15:50:58 +0000

    conversion code from William

--- run.py
+++ run.py
@@ -13,11 +13,43 @@ from collections import defaultdict, namedtuple
 from os import chdir
 from time import perf_counter
 
-def get_glif3_param_val_vars(components_dir, dynamics_params):
-    with open(os.path.join(components_dir, dynamics_params)) as f:
+def get_glif3_param_val_vars(cfg, dynamics_params):
+    with open(os.path.join(cfg.point_neuron_models_dir, dynamics_params)) as f:
         dynamics_params = json.load(f)
-    print(dynamics_params)
-    return {}, {}
+    
+    assert len(dynamics_params["asc_init"]) == 2
+    assert len(dynamics_params["asc_amps"]) == 2
+    assert len(dynamics_params["asc_decay"]) == 2
+
+    asc_decay = np.asarray(dynamics_params["asc_decay"])
+    r = np.asarray([1.0, 1.0])  # NEST default
+    asc_decay_rates = np.exp(-asc_decay * cfg.dt)
+    asc_stable_coeff = (1.0 / asc_decay / cfg.dt) * (1.0 - asc_decay_rates)
+    asc_refractory_decay_rates = r * np.exp(-asc_decay * dynamics_params["t_ref"])
+    
+    param_vals = {
+        "C": dynamics_params["C_m"] / 1000,  # pF -> nF
+        "G": dynamics_params["g"] / 1000,  # nS -> uS
+        "El": dynamics_params["E_L"],
+        "spike_cut_length": round(dynamics_params["t_ref"] / cfg.dt),
+        "th_inf": dynamics_params["V_th"],
+        "V_reset": dynamics_params["V_reset"],  # BMTK rounds to 3rd decimal
+        "asc_amp_array_1": dynamics_params["asc_amps"][0] / 1000,  # pA->nA
+        "asc_amp_array_2": dynamics_params["asc_amps"][1] / 1000,  # pA->nA
+        "asc_stable_coeff_1": asc_stable_coeff[0],
+        "asc_stable_coeff_2": asc_stable_coeff[1],
+        "asc_decay_rates_1": asc_decay_rates[0],
+        "asc_decay_rates_2": asc_decay_rates[1],
+        "asc_refractory_decay_rates_1": asc_refractory_decay_rates[0],
+        "asc_refractory_decay_rates_2": asc_refractory_decay_rates[1]}
+    
+    var_vals = {
+        "V": dynamics_params["V_m"],
+        "refractory_countdown": -1,
+        "ASC_1": dynamics_params["asc_init"][0] / 1000,  # pA -> nA
+        "ASC_2": dynamics_params["asc_init"][1] / 1000}  # pA -> nA
+
+    return param_vals, var_vals
 
 node_id_lookup_dtype = np.dtype([("id", np.uint32), ("index", np.uint32)])
 
@@ -49,6 +81,7 @@ for nodes, node_types in node_files:
     for name, pop in nodes["nodes"].items():
         print(f"\t{name}")
         # Build dataframe from required node data
+        # **TODO** check that all model_template are nest:glif_psc
         pop_df = pd.DataFrame(data={"node_type_id": pop["node_type_id"]},
                               index=pop["node_id"])
         pop_df = pop_df.join(node_types, on="node_type_id")
@@ -130,12 +163,19 @@ for pop_name, pops in pop_node_dict.items():
     # Loop through homogeneous GeNN populations within this
     for pop_nodes, pop_grouping in pops:
         assert len(pop_grouping) > 0
-        print(f"{pop_name}_{pop_grouping[0]}")
+        genn_pop_name = f"{pop_name}_{pop_grouping[0]}"
+        print(genn_pop_name)
         
         # If population has dynamics
         if len(pop_grouping) == 2:
             print("\tPoint process")
-            param_vals, var_vals = get_glif3_param_val_vars(
-                cfg.point_neuron_models_dir, pop_grouping[1])
+            
+            # Get GeNN 
+            param_vals, var_vals = get_glif3_param_val_vars(cfg, pop_grouping[1])
+            
+            
+            genn_pop = model.add_neuron_population(
+                genn_pop_name, len(pop_nodes), genn_models.glif3,
+                param_vals, var_vals)
         else:
             print("\tVirtual")
