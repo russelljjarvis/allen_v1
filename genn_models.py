5e8dfeee6891b9d5eabc70c5072534ab55396a15
neworderofjamie <neworderofjamie@gmail.com>
2023-02-14 15:30:55 +0000

    GLIF and alpha PSC model

---
+++ genn_models.py
@@ -0,0 +1,65 @@
+from pygenn.genn_model import (create_custom_neuron_class,
+                               
+glif3 = pygenn.genn_model.create_custom_neuron_class(
+    "glif3",
+    param_names=[
+        "C",
+        "G",
+        "El",
+        "spike_cut_length",
+        "th_inf",
+        "V_reset",
+        "asc_amp_array_1",
+        "asc_amp_array_2",
+        "asc_stable_coeff_1",
+        "asc_stable_coeff_2",
+        "asc_decay_rates_1",
+        "asc_decay_rates_2",
+        "asc_refractory_decay_rates_1",
+        "asc_refractory_decay_rates_2"],
+    var_name_types=[
+        ("V", "scalar"),
+        ("refractory_countdown", "int"),
+        ("ASC_1", "scalar"),
+        ("ASC_2", "scalar")],
+    sim_code="""
+
+    // Sum after spike currents
+    const scalar sum_of_ASC = $(ASC_1)*$(asc_stable_coeff_1) + $(ASC_2)*$(asc_stable_coeff_2);
+
+    // Voltage
+    if ($(refractory_countdown) <= 0) {
+        $(V)+=1/$(C)*($(Isyn)+sum_of_ASC-$(G)*($(V)-$(El)))*DT;
+    }
+
+    // ASCurrents
+    if ($(refractory_countdown) <= 0) {
+        $(ASC_1) *= $(asc_decay_rates_1);
+        $(ASC_2) *= $(asc_decay_rates_2);
+        }
+
+
+    // Decrement refractory_countdown by 1; Do not decrement past -1
+    if ($(refractory_countdown) > -1) {
+        $(refractory_countdown) -= 1;
+    }
+    """,
+    threshold_condition_code="$(V) > $(th_inf)",
+    reset_code="""
+    $(V)=$(V_reset);
+    $(ASC_1) = $(asc_amp_array_1) + $(ASC_1) * $(asc_refractory_decay_rates_1);
+    $(ASC_2) = $(asc_amp_array_2) + $(ASC_2) * $(asc_refractory_decay_rates_2);
+    $(refractory_countdown) = $(spike_cut_length);
+    """)
+
+psc_alpha = pygenn.genn_model.create_custom_postsynaptic_class(
+    class_name="Alpha",
+    decay_code="""
+    $(x) = exp(-DT/$(tau)) * ((DT * $(inSyn) * exp(1.0f) / $(tau)) + $(x));
+    $(inSyn)*=exp(-DT/$(tau));
+    """,
+    apply_input_code="""
+    $(Isyn) += $(x);     
+    """,
+    var_name_types=[("x", "scalar")],
+    param_names=[("tau")])
