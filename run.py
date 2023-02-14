5adca9ad008be53f149ec30f30495c00742eb414
neworderofjamie <neworderofjamie@gmail.com>
2023-02-14 12:14:17 +0000

    switched to plain h5py and pandas rather than using pySONATA

--- run.py
+++ run.py
@@ -1,14 +1,22 @@
-from collections import defaultdict
+from collections import defaultdict, namedtuple
 from os import chdir
 from time import perf_counter
 
-from sonata.circuit import File
+from h5py import File
+#from sonata.circuit import File
 from sonata.config import SonataConfig
+
 
 import numpy as np
 import pandas as pd
 
 import pygenn
+
+
+node_to_pop_id_dtype = np.dtype([("location", object), ("dynamics_params", object), 
+                                 ("pop_index", np.uint32)])
+
+edge_type_dtype = np.dtype([("delay", float), ("dynamics_params", object)])
 
 # Open top-level Sonata configu
 chdir("v1_point")
@@ -20,105 +28,107 @@ model.dT = cfg.dt
 
 # Open files, specified by config
 # **NOTE** IDs overlap
-node_files = [File(data_files=n["nodes_file"], 
-                   data_type_files=n["node_types_file"])
+# **TODO** reduce width of datatypes
+node_files = [(File(n["nodes_file"], "r"), 
+               pd.read_csv(n["node_types_file"], index_col="node_type_id", 
+                           usecols=lambda n: n in ["node_type_id", "dynamics_params", "location"],
+                           dtype={"node_type_id": np.uint32, "dynamics_params": object},
+                           delimiter=" ", skipinitialspace=True))
               for n in cfg.networks["nodes"]]
 
-edge_files = [File(data_files=n["edges_file"],
-                   data_type_files=n["edge_types_file"],
-                   require_magic=False)
+edge_files = [(File(n["edges_file"], "r"),
+               pd.read_csv(n["edge_types_file"], index_col="edge_type_id", 
+                           usecols=lambda n: n in ["edge_type_id", "delay", "dynamics_params"],
+                           dtype={"edge_type_id": np.uint32, "delay": np.float32, "dynamics_params": object},
+                           delimiter=" ", skipinitialspace=True))
               for n in cfg.networks["edges"]]
-
-node_to_pop_id_dtype = np.dtype([("location", object), ("dynamics_params", object), 
-                                 ("pop_id", np.uint32)])
-
-edge_type_dtype = np.dtype([("delay", float), ("dynamics_params", object)])
 
 # Loop through node files
 print("Nodes")
 pop_node_dict = {}
-node_to_pop_id = {}
-for f in node_files:
+node_id_lookup = {}
+for nodes, node_types in node_files:
     # Loop through populations in each one
     # **NOTE** these aren't populations in the GeNN/PyNN sense
-    for pop in f.nodes.populations:
-        print(f"\t{pop.name}")
-
-        # If this population has layer annotations
-        node_df = pop.to_dataframe()
-        if "layer" in node_df:
+    for name, pop in nodes["nodes"].items():
+        print(f"\t{name}")
+        
+        # If node has dynamics ie is 'real' neuron
+        # **TODO** something model_type based
+        if "dynamics_params" in node_types:
+            # Build dataframe from required node data
+            pop_df = pd.DataFrame(data={"node_type_id": pop["node_type_id"]},
+                                  index=pop["node_id"])
+            pop_df = pop_df.join(node_types, on="node_type_id")
+            
+            
+            # Group nodes by location and dynamic parameter types and build dictionary
+            # of (location, dynamic params) tuples to numpy array of node IDs
             start_time = perf_counter()
-
-            # Group nodes by layer and dynamic parameter types and build dictionary
-            # of (layer, dynamic params) tuples to numpy array of node IDs
-            pop_node_dict[pop.name] = [(l, d, df["node_id"].to_numpy())
-                                       for (l, d), df in node_df.groupby(["location", "dynamics_params"])]
-            print(f"\t\t{len(pop_node_dict[pop.name])} homogeneous populations")
+            pop_node_dict[name] = [(l, d, df.index.to_numpy())
+                                   for (l, d), df in pop_df.groupby(["location", "dynamics_params"])]
+            print(f"\t\t{len(pop_node_dict[name])} homogeneous populations")
             print(f"\t\tBuilt dictionary in {perf_counter() - start_time} seconds")
 
             # Create empty array to map node IDs to populations and IDs within them
             start_time = perf_counter()
-            node_to_pop_id[pop.name] = np.empty(len(node_df), dtype=node_to_pop_id_dtype)
+            node_id_lookup[name] = np.empty(len(pop_df), dtype=node_to_pop_id_dtype)
 
             # Loop through newly-identified homogeneous populations and 
-            for l, d, nodes in pop_node_dict[pop.name]:
-                node_to_pop_id[pop.name]["location"][nodes] = l
-                node_to_pop_id[pop.name]["dynamics_params"][nodes] = d
-                node_to_pop_id[pop.name]["pop_id"][nodes] = nodes - nodes[0]
+            # **TODO** abstract away unique variables into index so this works with virtual nodes
+            for l, d, nodes in pop_node_dict[name]:
+                node_id_lookup[name]["location"][nodes] = l
+                node_id_lookup[name]["dynamics_params"][nodes] = d
+                node_id_lookup[name]["pop_index"][nodes] = nodes - nodes[0]
 
             print(f"\t\tBuilt ID lookup in {perf_counter() - start_time} seconds")
 
 # Loop through edge files
 print("Edges")
-edge_source_dict = {}
-edge_target_dict = {}
-for f in edge_files:
+pop_edge_dict = {}
+for edges, edge_types in edge_files:
     # Loop through populations in each one
     # **NOTE** these aren't populations in the GeNN/PyNN sense
-    for pop in f.edges.populations:
-        print(f"\t{pop.name} ({pop.source_population}->{pop.target_population})")
-
-        # Get edge types as Pandas dataframe
-        edge_types_df = pop.edge_types_table.to_dataframe()
-        #print(edge_types_df
-
-        # Check that they start at zero and are contiguous
-        assert edge_types_df.index[0] == 0
-        assert edge_types_df.index[-1] == (len(edge_types_df) - 1)
-
-
-        edge_type_map = np.empty(len(edge_types_df), dtype=edge_type_dtype)
-        for (d, p), df in edge_types_df.groupby(["delay", "dynamics_params"]):
-            edge_type_map["delay"][df.index] = d
-            edge_type_map["dynamics_params"][df.index] = p
-
-        #assert False
-        # **TODO** group edge type ids by dynamics_params i.e. receptor
-        # Get node to pop ID lookup table for source and target populations
-        source_node_to_pop_id = node_to_pop_id[pop.source_population]
-        target_node_to_pop_id = node_to_pop_id[pop.target_population]
-
-        # Loop through edges
+    for name, pop in edges["edges"].items():
+        source_node_pop = pop["source_node_id"].attrs["node_population"]
+        target_node_pop = pop["target_node_id"].attrs["node_population"]
+        print(f"\t{name} ({source_node_pop}->{target_node_pop})")
         start_time = perf_counter()
+        
+        # Lookup source and target nodes
+        source_nodes = node_id_lookup[source_node_pop][:][pop["source_node_id"][()]]
+        target_nodes = node_id_lookup[target_node_pop][:][pop["target_node_id"][()]]
+        
+        # Build dataframe from required edge data
+        pop_df = pd.DataFrame(data={"edge_group_id": pd.Series(pop["edge_group_id"], dtype=np.uint32),
+                                    "edge_group_index": pd.Series(pop["edge_group_index"], dtype=np.uint32), 
+                                    "edge_type_id": pd.Series(pop["edge_type_id"], dtype=np.uint32),
+                                    "source_pop_index": pd.Series(source_nodes["pop_index"], dtype=np.uint32),
+                                    "source_location": source_nodes["location"],
+                                    "source_dynamics_params": source_nodes["dynamics_params"],
+                                    "target_pop_index": pd.Series(target_nodes["pop_index"], dtype=np.uint32),
+                                    "target_location": target_nodes["location"],
+                                    "target_dynamics_params": target_nodes["dynamics_params"],})
 
-        # Remap source and target node IDs into layer, dynamics and population ID
-        # **YUCK** not sure how to do this within delving into internals
-        source_pop_id = source_node_to_pop_id[:][pop._source_node_id_ds[()]]
-        target_pop_id = target_node_to_pop_id[:][pop._target_node_id_ds[()]]
+        # Group by group ID
+        for group_id, pop_group_df in pop_df.groupby("edge_group_id"):
+            print(f"\t\tGroup {group_id}")
 
-        edge_type = edge_type_map[:][pop._pop_group["edge_type_id"][()]]
-        edge_df = pd.DataFrame(data={"edge_delay": edge_type["delay"],
-                                     "edge_dynamics_params": edge_type["dynamics_params"],
-                                     "source_location": source_pop_id["location"],
-                                     "source_dynamics_params": source_pop_id["dynamics_params"],
-                                     "source_pop_id": source_pop_id["pop_id"],
-                                     "target_location": target_pop_id["location"],
-                                     "target_dynamics_params": target_pop_id["dynamics_params"],
-                                     "target_pop_id": target_pop_id["pop_id"]})
-        edge_source_dict[pop.name] = [grp + (df["source_pop_id"].to_numpy(), df["target_pop_id"].to_numpy())
-                                      for grp, df in edge_df.groupby(["edge_delay", "edge_dynamics_params", 
-                                                                      "source_location", "source_dynamics_params",
-                                                                      "target_location", "target_dynamics_params"])]
+            # Build dataframe from required group data
+            group = pop[str(group_id)]
+            group_df = pd.DataFrame(data={"syn_weight": pd.Series(group["syn_weight"], dtype=np.float32)})
 
-        print(f"\t\tlen(edge_df) edges in {len(edge_source_dict[pop.name])} homogeneous populations")
+            # Join with pop_df
+            # **THINK** is this right sort of join
+            pop_group_df = pop_group_df.join(group_df, on="edge_group_index")
+            
+            # Join with edge types
+            pop_group_df = pop_group_df.join(edge_types, on="edge_type_id")
+            
+            pop_edge_dict[name] = [grp + (df["source_pop_index"].to_numpy(), df["target_pop_index"].to_numpy())
+                                   for grp, df in pop_group_df.groupby(["delay", "dynamics_params", 
+                                                                        "source_location", "source_dynamics_params",
+                                                                        "target_location", "target_dynamics_params"])]
+
+            print(f"\t\t\tlen(pop_group_df) edges in {len(pop_edge_dict[name])} homogeneous populations")
         print(f"\t\tBuilt edge dictionaries in {perf_counter() - start_time} seconds")
